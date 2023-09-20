c2d9f838972692e48dbe033ed8627d54a08695df
Tomas Prajzler <tomas.prajzler@email.cz>
2023-09-20 18:05:40 +0200

    create method for returning board in webcontroller

--- src/main/java/com/example/monopoly/controllers/WebController.java
+++ src/main/java/com/example/monopoly/controllers/WebController.java
@@ -1,7 +1,12 @@
 package com.example.monopoly.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class WebController {
+    @GetMapping("/play")
+    public String play(){
+        return "board";
+    }
 }
